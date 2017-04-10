4f711d74d8c428c25c8e9ccacb7b4667a8325e58
Ajmal <ajmal.basheer@lxisoft.com>
2017-04-10 16:41:31 +0530

    initial

---
+++ demo/src/main/java/com/lxisoft/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.lxisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@RequestMapping("/a")
+	public String getDate(){
+		return "ajmal";
+	}
+	@RequestMapping("/b")
+	public String getData(){
+		return "diviso";
+	}
+}
